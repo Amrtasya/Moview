944181f4ec371a71b707f9937c1f418a4b59dda5
rahma <fauziahrahma138@gmail.com>
2026-06-23 15:42:22 +0800

    Add DiaryLogsActivity and activity_diary_logs layout

---
+++ app/src/main/java/com/example/moviewapp/ui/DiaryLogsActivity.java
@@ -0,0 +1,16 @@
+package com.example.moviewapp.ui;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.moviewapp.R;
+
+public class DiaryLogsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_diary_logs);
+    }
+}
